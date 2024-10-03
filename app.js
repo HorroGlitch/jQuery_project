af0ab08d9509be4c09c6cf03fd18e452ea8563e5
HorroGlitch <lverot@edenschool.fr>
2024-10-03 14:50:57 +0200

    calling api and getting informations

--- app.js
+++ app.js
@@ -40,8 +40,24 @@
 //   });
 
 
-async function afficherFilms() {
-    const reponse = await fetch("https://api.openweathermap.org/data/2.5/forecast?q=${nom}&APPID=0eee845fbfb08dcc0204fcb70613dc4a");
-    const films = await reponse.json();
-    console.log(films);
+let ville = document.querySelector("h2")
+let pays = document.querySelector("h3")
+let nomDeVille = document.querySelector("#nomDeVille")
+let submitIt = document.querySelector("#submitVille")
+
+//On the top, I get 
+
+async function town() {
+    const reponse = await fetch("https://api.openweathermap.org/data/2.5/forecast?q=lion&APPID=0eee845fbfb08dcc0204fcb70613dc4a");
+    const nom = await reponse.json();
+    console.log(nom)
+    let minTemp = nom.dt.temp_min.weather[0].description
+    console.log(minTemp)
+
+    // const temp = new Promise
   }
+  town()
+
+  submitIt.addEventListener("click", ()=>{
+    ville.textContent = nomDeVille.value
+  })
